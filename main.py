37f482eb37f23221ab698ddfc2ee519ba1d90404
RandomPerson238 <babyed.han@gmail.com>
2026-01-30 19:25:49 -0800

    added buzzing system

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Main script to download and process science bowl questions for qbreader
 # Copyright (C) 2025-2026 Edward Han
-#
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
+
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -141,7 +141,7 @@ class multiple_choice:
     x: str
     y: str
     z: str
-    correct: str
+    answer: str
 
 @dataclass
 class short_answer:
